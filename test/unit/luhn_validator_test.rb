868de01f6f07340514a68ea34ce4a8e118fb0c01
lzbikowski <luke.zbikowski@intecco.com.au>
2013-03-20 03:58:44 +1100

    Added test for odd length card. Fixed negative scenario.

--- test/unit/luhn_validator_test.rb
+++ test/unit/luhn_validator_test.rb
@@ -10,9 +10,15 @@ class LuhnValidatorTest < Test::Unit::TestCase
     def test_visa_card_valid
         assert(Credio::Validators::LuhnValidator::valid?('4408041234567893'), 'Incorrect validation for VISA 4408 0412 3456 7893')
     end
+
+    # check luhn validator number
+    def test_amex_card_valid_odd_length
+        assert(Credio::Validators::LuhnValidator::valid?('378282246310005'), 'Incorrect validation for AMEX 378 2822 4631 0005')
+    end
     
     # check luhn validator number
     def test_visa_card_invalid
-        assert(Credio::Validators::LuhnValidator::valid?('4417123456789112'), 'Incorrect validation for VISA 4417 1234 5678 9112')
+        assert(! Credio::Validators::LuhnValidator::valid?('4417123456789112'), 'Incorrect validation for VISA 4417 1234 5678 9112')
     end
+    
 end
